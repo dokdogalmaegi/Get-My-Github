b953b1b31c51dfb14c5913cfe52afa6680cfb2e2
임준영 <clik202012@gmail.com>
2021-10-28 21:40:06 +0900

    Add Menu multilingual

--- src/popup/component/Menu.tsx
+++ src/popup/component/Menu.tsx
@@ -1,7 +1,8 @@
 import { AppBar, IconButton, makeStyles, Toolbar, Typography, Button } from '@material-ui/core';
 import Home from '@material-ui/icons/Home';
-import koreaJson from '../../textJson/korea.json';
-import React from 'react';
+import korean from '../../textJson/korea.json';
+import english from '../../textJson/english.json';
+import React, { useState } from 'react';
 
 const useStyles = makeStyles({
   toolBar: {
@@ -11,19 +12,20 @@ const useStyles = makeStyles({
   }
 })
 
-export default function Menu({ goHomeCb }) {
+export default function Menu({ goHomeCb, selectLang, changeLang }) {
   const classes = useStyles();
 
   return (
     <AppBar position="static">
       <Toolbar variant="dense" className={classes.toolBar}>
         <Typography variant="h6">
-          { koreaJson.title }
+          { selectLang.title }
         </Typography>
         <IconButton color="primary" component="span" onClick={ goHomeCb }>
           <Home style={{ color: 'white' }} />
         </IconButton>
-
+        {selectLang == korean && <button onClick={ changeLang }>{ selectLang.language }</button>}
+        {selectLang == english && <button onClick={ changeLang }>{ selectLang.language }</button>}
       </Toolbar>
     </AppBar>
   );
